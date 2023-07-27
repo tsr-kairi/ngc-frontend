86947a86492315e96d5049b14bc9a4084428d4ff
swaraj bachu <rajswaraj.r@gmail.com>
2023-07-27 12:21:41 +0530

    fix(): fixed styles along with salary revision done

--- src/components/layout/DashboardLayout.tsx
+++ src/components/layout/DashboardLayout.tsx
@@ -48,7 +48,6 @@ function DashboardLayout() {
           style={{
             flex: 1,
             maxWidth: 'calc(100vw - 350px)',
-            maxHeight: 'calc(100vh - 80px)',
             overflow: 'scroll',
           }}
         >
--- src/pages/salary/revisionview/index.tsx
+++ src/pages/salary/revisionview/index.tsx
@@ -1,3 +1,103 @@
+import { Box, Text } from '@mantine/core';
+import SalaryTable from './Table';
+
+const salary = [
+  {
+    SalaryComponent: 'Basic',
+    currentMonthly: 10000,
+    currentAnnual: 120000,
+    revisedMonthly: 20000,
+    revisedAnnual: 240000,
+  },
+  {
+    SalaryComponent: 'HRA',
+    currentMonthly: 10000,
+    currentAnnual: 120000,
+    revisedMonthly: 20000,
+    revisedAnnual: 240000,
+  },
+  {
+    SalaryComponent: 'Conveyance',
+    currentMonthly: 10000,
+    currentAnnual: 120000,
+    revisedMonthly: 20000,
+    revisedAnnual: 240000,
+  },
+  {
+    SalaryComponent: 'Special Allowance',
+    currentMonthly: 10000,
+    currentAnnual: 120000,
+    revisedMonthly: 20000,
+    revisedAnnual: 240000,
+  },
+  {
+    SalaryComponent: 'LTA',
+    currentMonthly: 10000,
+    currentAnnual: 120000,
+    revisedMonthly: 20000,
+    revisedAnnual: 240000,
+  },
+  {
+    SalaryComponent: 'ESIC (Employer)',
+    currentMonthly: 10000,
+    currentAnnual: 120000,
+    revisedMonthly: 20000,
+    revisedAnnual: 240000,
+  },
+];
+
+const deductions = [
+  {
+    SalaryComponent: 'ESIC (Employee)',
+    currentMonthly: 12.011,
+    currentAnnual: 144.132,
+    revisedMonthly: 12.011,
+    revisedAnnual: 144.132,
+  },
+  {
+    SalaryComponent: 'ESIC (Employer)',
+    currentMonthly: 12.011,
+    currentAnnual: 144.132,
+    revisedMonthly: 12.011,
+    revisedAnnual: 144.132,
+  },
+  {
+    SalaryComponent: 'EPF (Employee)',
+    currentMonthly: 12.011,
+    currentAnnual: 144.132,
+    revisedMonthly: 12.011,
+    revisedAnnual: 144.132,
+  },
+  {
+    SalaryComponent: 'EPF (Employer)',
+    currentMonthly: 12.011,
+    currentAnnual: 144.132,
+    revisedMonthly: 12.011,
+    revisedAnnual: 144.132,
+  },
+  {
+    SalaryComponent: 'Professional Tax',
+    currentMonthly: 12.011,
+    currentAnnual: 144.132,
+    revisedMonthly: 12.011,
+    revisedAnnual: 144.132,
+  },
+  {
+    SalaryComponent: 'TDS',
+    currentMonthly: 12.011,
+    currentAnnual: 144.132,
+    revisedMonthly: 12.011,
+    revisedAnnual: 144.132,
+  },
+];
+
 export default function SalaryRevisionView() {
-  return <div />;
+  return (
+    <Box>
+      <Text fw={400} size={30}>
+        Salary Revision View
+      </Text>
+      <SalaryTable salarys={salary} deductions={deductions} />
+    </Box>
+  );
 }
