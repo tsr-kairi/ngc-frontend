ff7b3badaf3aef0bacb48db3c4defb4eb76ddf6f
swaraj bachu <rajswaraj.r@gmail.com>
2023-08-02 10:43:42 +0530

    feat(): table

--- src/pages/leaveManagementSide/leaveManagement/index.tsx
+++ src/pages/leaveManagementSide/leaveManagement/index.tsx
@@ -1,12 +1,18 @@
-import { Box } from '@mantine/core';
+import { Loader } from '@mantine/core';
+import useGetAllLeaveManagement from '../hooks/leaveManagement';
 import LeaveTable from './table';
 
 function LeaveManagementManager() {
-  return (
-    <Box>
-      <LeaveTable />
-    </Box>
-  );
+  const { data, isError, isLoading } = useGetAllLeaveManagement();
+
+  if (isError) {
+    return <h1>An Error Occurred</h1>;
+  }
+
+  if (!isLoading) {
+    return <LeaveTable data={data?.data ?? []} />;
+  }
+  return <Loader variant="dots" />;
 }
 
 export default LeaveManagementManager;
--- src/pages/leaveManagementSide/leaveManagement/table.tsx
+++ src/pages/leaveManagementSide/leaveManagement/table.tsx
@@ -1,122 +1,211 @@
-/* eslint-disable react/no-unstable-nested-components */
-/* eslint-disable react/jsx-pascal-case */
-/* eslint-disable react/prop-types */
-import { Badge, Box, Text } from '@mantine/core';
+/* eslint-disable no-nested-ternary */
+/* eslint-disable react/require-default-props */
+import { ListViewLayout } from '@/components/layout/listView.layout';
+import { LeaveManagement } from '@/types/LeaveManagement-type';
 import {
-  MRT_Table,
-  useMantineReactTable,
-  type MRT_ColumnDef,
-} from 'mantine-react-table';
-import { useMemo } from 'react';
-import data from './leave';
+  Badge,
+  Menu,
+  Text,
+  Tooltip,
+  UnstyledButton,
+  createStyles,
+} from '@mantine/core';
+import { IconDotsVertical, IconEdit, IconTrash } from '@tabler/icons-react';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
-function LeaveTable() {
-  const columns = useMemo<MRT_ColumnDef<LeaveManagementManager>[]>(
-    () => [
-      {
-        accessorKey: 'transaction',
-        header: 'Transaction',
-        Cell: ({ cell }) => {
-          const grant = cell.getValue() as string;
-          if (grant === 'Granted') {
-            return (
-              <Badge color="green" variant="outline" p="sm">
-                Granted
-              </Badge>
-            );
-          }
-          if (grant === 'Availed') {
-            return (
-              <Badge color="red" variant="outline" p="sm">
-                Availed
-              </Badge>
-            );
-          }
-          return null;
-        },
-      },
-      {
-        accessorKey: 'transactionDate',
-        header: 'Transaction Date',
-      },
-      {
-        accessorKey: 'leaveType',
-        header: 'Leave Type',
-        Cell: ({ cell }) => {
-          const leave = cell.getValue() as string;
-          if (leave === 'Earned') {
-            return (
-              <Badge color="green" variant="outline" p="sm">
-                Earned
-              </Badge>
-            );
-          }
-          if (leave === 'Sick') {
-            return (
-              <Badge color="black" variant="outline" p="sm">
-                Sick
-              </Badge>
-            );
-          }
-          if (leave === 'Casual') {
-            return (
-              <Badge color="red" variant="outline" p="sm">
-                Casual
-              </Badge>
-            );
-          }
-          return null;
-        },
-      },
-      {
-        accessorKey: 'fromDate',
-        header: 'From Date',
-      },
-      {
-        accessorKey: 'toDate',
-        header: 'To Date',
-      },
-      {
-        accessorKey: 'days',
-        header: 'Days',
-      },
-      {
-        accessorKey: 'expiryDate',
-        header: 'Expiry Date',
-      },
-    ],
-    []
-  );
-  const table = useMantineReactTable({
-    columns,
-    data,
-    enableColumnActions: false,
-    enableColumnFilters: false,
-    enablePagination: true,
-    enableSorting: false,
-    mantineTableProps: {
-      highlightOnHover: false,
-      withColumnBorders: false,
+// Style for the entire ListViewLayoutPage Page
+const useStyles = createStyles((theme) => ({
+  th: {
+    padding: '0 !important',
+  },
+
+  LeaveManagementRowData: {
+    border: 'none',
+    '&:hover': {
+      backgroundColor:
+        theme.colorScheme === 'dark'
+          ? theme.colors.dark[6]
+          : theme.colors.gray[1],
     },
-  });
-  return (
-    // <ScrollArea
-    //   h="56vh"
-    //   offsetScrollbars
-    //   styles={() => ({
-    //     scrollbar: {
-    //       height: '300px',
-    //     },
-    //   })}
-    // >
-    <Box mt="40px" sx={{ width: 'calc(100vw - 300px)' }}>
-      <Text fz="lg" weight={700} mb="md">
-        Leave Transaction History
-      </Text>
-      <MRT_Table table={table} />
-    </Box>
-    // </ScrollArea>
-  );
+  },
+
+  icon: {
+    width: 21,
+    height: 21,
+    borderRadius: 21,
+  },
+  editIcon: {
+    color: theme.colors.brand[6],
+    '&:hover': {
+      color: theme.colors.dark[6],
+    },
+  },
+  deleteIcon: {
+    color: '#FF7676',
+    '&:hover': {
+      color: '#FF1414',
+    },
+  },
+  user: {},
+  userActive: {},
+  menuItem: {
+    '&:hover': {
+      backgroundColor: theme.colors.brand[9],
+    },
+  },
+  userLink: {
+    textDecoration: 'none',
+    '&:hover': {
+      color: theme.colors.brand[6],
+    },
+  },
+}));
+
+interface ILeaveManagementProps {
+  data: LeaveManagement[];
 }
 
+// Rendering the LeaveManagementTable Component
+function LeaveTable({ data }: ILeaveManagementProps) {
+  const { classes, cx } = useStyles();
+  const [userMenuOpened, setUserMenuOpened] = useState(false);
+
+  // Table Rows getElement component management
+  const rows = data?.map((row) => (
+    <tr key={row?.uuid} className={classes.LeaveManagementRowData}>
+      <td>
+        <Menu
+          width={230}
+          onClose={() => setUserMenuOpened(false)}
+          onOpen={() => setUserMenuOpened(true)}
+          offset={14}
+        >
+          <Menu.Target>
+            <UnstyledButton
+              className={cx(classes.user, {
+                [classes.userActive]: userMenuOpened,
+              })}
+            >
+              <Tooltip label="Action" color="blue" withArrow>
+                <IconDotsVertical size={16} cursor="pointer" />
+              </Tooltip>
+            </UnstyledButton>
+          </Menu.Target>
+          <Menu.Dropdown>
+            <Menu.Label>Take your action carefully</Menu.Label>
+
+            <Menu.Item
+              icon={
+                <IconEdit
+                  size={14}
+                  // stroke={1.5}
+                  className={classes.editIcon}
+                />
+              }
+              className={classes.menuItem}
+            >
+              Edit LeaveManagement
+            </Menu.Item>
+            <Menu.Item
+              icon={
+                <IconTrash
+                  size={14}
+                  // stroke={1.5}
+                  className={classes.deleteIcon}
+                />
+              }
+              className={classes.menuItem}
+            >
+              Delete LeaveManagement
+            </Menu.Item>
+          </Menu.Dropdown>
+        </Menu>
+      </td>
+      <td>{row?.applicationDate ? row?.applicationDate : ''}</td>
+      <td>
+        <Link
+          to={`/LeaveManagement-details/${row?.uuid}`}
+          className={classes.userLink}
+        >
+          {row?.leaveType === 'Casual' ? (
+            <Badge variant="outline" color="green">
+              Casual
+            </Badge>
+          ) : row?.leaveType === 'Earned' ? (
+            <Badge variant="outline" color="red">
+              Earned
+            </Badge>
+          ) : row?.leaveType === 'Sick' ? (
+            <Badge variant="outline" color="yellow">
+              Sick
+            </Badge>
+          ) : null}
+        </Link>
+      </td>
+      <td>{row?.fromDate ? row?.fromDate : ''}</td>
+      <td>{row?.toDate ? row?.toDate : ''}</td>
+      <td>{row?.days ? row?.days : ''}</td>
+      <td>
+        {row?.status === 'Approved' ? (
+          <Badge variant="outline" color="green">
+            Approved
+          </Badge>
+        ) : row?.status === 'Pending' ? (
+          <Badge variant="outline" color="yellow">
+            Pending
+          </Badge>
+        ) : row?.status === 'Rejected' ? (
+          <Badge variant="outline" color="red">
+            Rejected
+          </Badge>
+        ) : null}
+      </td>
+    </tr>
+  ));
+
+  // Returning the Scroll Area as ListViewLayout of Table
+  return (
+    <ListViewLayout
+      title="Leave Managements"
+      isError={false}
+      isLoading={false}
+      hidePlusButton
+    >
+      <thead>
+        <tr>
+          <th>
+            <b>Action</b>
+          </th>
+          <th>
+            <b>Application Date</b>
+          </th>
+          <th>
+            <b>Leave Type</b>
+          </th>
+          <th>
+            <b>From Date</b>
+          </th>
+          <th>
+            <b>To Date</b>
+          </th>
+          <th>
+            <b>Days</b>
+          </th>
+          <th>
+            <b>Status</b>
+          </th>
+        </tr>
+      </thead>
+
+      {rows?.length > 0 ? (
+        <tbody>{rows}</tbody>
+      ) : (
+        <Text weight={500} align="center" p={8}>
+          No records found
+        </Text>
+      )}
+    </ListViewLayout>
+  );
+}
 export default LeaveTable;
